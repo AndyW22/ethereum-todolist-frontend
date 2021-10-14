5131142bef0a153275d81533bf4fecc9c67e293a
AndyW22 <andrew.waller24@outlook.com>
2021-10-14 15:37:29 +0100

    Added network + account fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
-  return <div className='container'></div>;
+  const [account, setAccount] = useState<string | null>(null);
+  const [network, setNetwork] = useState<string | null>(null);
+  useEffect(() => {
+    const loadBlockchainData = async () => {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const network = await web3.eth.net.getNetworkType();
+      const accounts = await web3.eth.getAccounts();
+      console.log(accounts);
+      setNetwork(network);
+      setAccount(accounts[0]);
+    };
+    loadBlockchainData();
+  }, []);
+
+  return (
+    <div className='container'>
+      <h1>Current Network: {network}</h1>
+      <h2>Account details:</h2>
+      <p>{account}</p>
+    </div>
+  );
 }
 
 export default App;
